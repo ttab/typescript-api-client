9f575d802ecb658620620df9ec6e5fb103976161
Fredrik Appelberg <fredrik.appelberg@tt.se>
2020-10-13 15:00:13 +0200

    typescript upgrade

--- generator/generate.ts
+++ generator/generate.ts
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 import { readFileSync, writeFileSync } from 'fs';
-import { camelCase, capitalize, filter, map, find } from 'lodash';
+import { camelCase, capitalize, filter, find, map } from 'lodash';
 import * as mustache from 'mustache';
 import { Swagger, SwaggerType } from 'swagger-typescript-codegen/lib/swagger/Swagger';
 import { convertType } from 'swagger-typescript-codegen/lib/typescript';
@@ -218,63 +218,68 @@ function buildView(spec: Swagger): View {
       }
     }
     let api: Api = apis[e.api]
-    for (let [httpMethod, method] of Object.entries(obj)) {
-      if (!httpMethod.startsWith('x-')) {
-        let parameters = buildParameters(method.parameters, spec)
-        let responseType = convertType(method.responses['200'], spec)
-        let name = shortName(httpMethod, e, responseType)
 
-        // check if there are other methods with the same shortName
-        let lookalikes = filter(api.methods, { shortName: name })
-        for (let m of lookalikes) {
-          m.isSingleton = false
-        }
+    for (let httpMethod of Object.keys(obj)) {
+      if (httpMethod === 'get' || httpMethod === 'post' || httpMethod === 'put' || httpMethod === 'delete') {
+        let method = obj[httpMethod]
+        if (method) {
 
-        let parameterNames = [
-          ...parameters.path.map(p => p.name),
-          parameters.query.length > 0 ? 'parameters' : null,
-          ...parameters.body.map(p => p.name)
-        ].filter(n => n)
+          let parameters = buildParameters(method.parameters, spec)
+          let responseType = convertType(method.responses['200'], spec)
+          let name = shortName(httpMethod, e, responseType)
 
-        let examples = [
-          ...parameters.path.map(p => p.example),
-          parameters.query.length > 0 ? `{${map(parameters.query, (p) => p.example ? `${p.name}: ${p.example}` : null).filter((e) => e).join(', ')}}` : null,
-          ...parameters.body.map(p => p.example)
-        ].filter(n => n)
+          // check if there are other methods with the same shortName
+          let lookalikes = filter(api.methods, { shortName: name })
+          for (let m of lookalikes) {
+            m.isSingleton = false
+          }
 
-        let options = []
-        let wait = find(parameters.query, { name: 'wait' })
-        if (wait) {
-          options.push({
-            name: 'timeout',
-            value: `parameters.wait ? parameters.wait * 1000 + 1000 : ${wait.default} * 1000 + 1000`
+          let parameterNames = [
+            ...parameters.path.map(p => p.name),
+            parameters.query.length > 0 ? 'parameters' : null,
+            ...parameters.body.map(p => p.name)
+          ].filter(n => n)
+
+          let examples = [
+            ...parameters.path.map(p => p.example),
+            parameters.query.length > 0 ? `{${map(parameters.query, (p) => p.example ? `${p.name}: ${p.example}` : null).filter((e) => e).join(', ')}}` : null,
+            ...parameters.body.map(p => p.example)
+          ].filter(n => n)
+
+          let options = []
+          let wait = find(parameters.query, { name: 'wait' })
+          if (wait) {
+            options.push({
+              name: 'timeout',
+              value: `parameters.wait ? parameters.wait * 1000 + 1000 : ${wait.default} * 1000 + 1000`
+            })
+          }
+
+          api.methods.push({
+            name: function() {
+              return this.isSingleton ? this.shortName : this.fullName
+            },
+            shortName: name,
+            summary: method.summary,
+            isSingleton: lookalikes.length === 0,
+            description: method.description,
+            fullName: fullName(name, parameters.path),
+            pathParameters: parameters.path,
+            queryParameters: parameters.query,
+            bodyParameters: parameters.body,
+            formatString: e.formatString,
+            httpMethod,
+            hasQueryParameters: parameters.query.length > 0,
+            hasBodyParameters: parameters.body.length > 0,
+            responseType,
+            parameterNames: parameterNames.join(', '),
+            anchor: function() {
+              return `${this.name()}${parameterNames.join('-')}`.toLowerCase()
+            },
+            examples: examples.join(', '),
+            options
           })
         }
-
-        api.methods.push({
-          name: function() {
-            return this.isSingleton ? this.shortName : this.fullName
-          },
-          shortName: name,
-          summary: method.summary,
-          isSingleton: lookalikes.length === 0,
-          description: method.description,
-          fullName: fullName(name, parameters.path),
-          pathParameters: parameters.path,
-          queryParameters: parameters.query,
-          bodyParameters: parameters.body,
-          formatString: e.formatString,
-          httpMethod,
-          hasQueryParameters: parameters.query.length > 0,
-          hasBodyParameters: parameters.body.length > 0,
-          responseType,
-          parameterNames: parameterNames.join(', '),
-          anchor: function() {
-            return `${this.name()}${parameterNames.join('-')}`.toLowerCase()
-          },
-          examples: examples.join(', '),
-          options
-        })
       }
     }
   }
